8d1fc20c895797ed967716d602af4d99687e9655
floissac <floissac.w@gmail.com>
2018-02-03 13:36:16 +0000

    new merge

--- src/js/config/router.config.js
+++ src/js/config/router.config.js
@@ -10,6 +10,21 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider) {
     .state('verif', {
       url: '/',
       templateUrl: 'js/views/cocktails/verif.html'
+    })
+    .state('cocktailsIndex', {
+      url: '/',
+      templateUrl: 'js/views/cocktails/index.html',
+      controller: 'CocktailsIndexCtrl as vm'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'js/views/auth/login.html',
+      controller: 'LoginCtrl as vm'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'js/views/auth/register.html',
+      controller: 'RegisterCtrl as vm'
     });
 
   $urlRouterProvider.otherwise('/');
