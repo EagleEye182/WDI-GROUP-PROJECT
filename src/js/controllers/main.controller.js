f76eb2f725bb31738ddea163fd35e12c40699489
Anish Vadher <avadher510@gmail.com>
2018-02-05 10:29:09 +0000

    main controller functions added > issue identified with delete show button > needs fixing

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -2,7 +2,31 @@ angular
   .module('cocktailApp')
   .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = [];
-function MainCtrl() {
-  console.log('in MainCtrl');
+MainCtrl.$inject = ['$transitions', '$rootScope', '$state', '$auth'];
+function MainCtrl($transitions, $rootScope, $state, $auth) {
+  const vm = this;
+  vm.isAuthenticated = $auth.isAuthenticated;
+
+  $rootScope.$on('error', (e, err) => {
+    //attaching API error to the MainCtrl
+    vm.message = err.data.message;
+    if (err.status === 401 && vm.pageName !== 'login') {
+      vm.stateHasChanged = false;
+      $state.go('login');
+    }
+
+  });
+
+  $transitions.onSuccess({}, (transition) => {
+    vm.menuIsOpen = false;
+    vm.pageName = transition.to().name;
+    if (vm.stateHasChanged) vm.message = null;
+    if (!vm.stateHasChanged) vm.stateHasChanged = true;
+  });
+
+  vm.logout = logout;
+  function logout() {
+    $auth.logout();
+    $state.go('home');
+  }
 }
